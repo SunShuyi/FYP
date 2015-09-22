1d342da29c0f8851c84c33d993afd9e370efcfaa
SunShuyi <prettycure_16@hotmail.com>
2015-09-22 15:00:18 +0800

    RotationEdited

--- FYP/Assets/Scripts/Minigame/C5/FindEumaeus's/C_cameraaaa.cs
+++ FYP/Assets/Scripts/Minigame/C5/FindEumaeus's/C_cameraaaa.cs
@@ -5,12 +5,70 @@ public class C_cameraaaa : MonoBehaviour {
 
 	public GameObject theOdysseusMG1 ;
 	float dist = 10.0f;
-	
+	public Rotate rotation;
 	// Update is called once per frame
 	void Update () {
+		switch (rotation.state) {
+		case 0:
+		{
+			DownOffSet();
+			break;
+		}
+		case 1:
+		{
+			RightOffSet();
+			break;
+		}
+		case 2:
+		{
+			UpOffSet();
+			break;
+		}
+		case 3:
+		{
+			LeftOffSet();
+			break;
+		}
+		default:
+		{
+			DownOffSet();
+			break;
+		}
+		}
+	}
+
+	void DownOffSet()
+	{
 		Vector3 temp = new Vector3 ();
 		temp.x = theOdysseusMG1.transform.position.x;
 		temp.y = theOdysseusMG1.transform.position.y + 3.0f;
+		temp.z = theOdysseusMG1.transform.position.z - dist;
+		transform.position = temp;
+	}
+
+	void UpOffSet()
+	{
+		Vector3 temp = new Vector3 ();
+		temp.x = theOdysseusMG1.transform.position.x;
+		temp.y = theOdysseusMG1.transform.position.y - 3.0f;
+		temp.z = theOdysseusMG1.transform.position.z - dist;
+		transform.position = temp;
+	}
+
+	void LeftOffSet()
+	{
+		Vector3 temp = new Vector3 ();
+		temp.x = theOdysseusMG1.transform.position.x + 3.0f;
+		temp.y = theOdysseusMG1.transform.position.y;
+		temp.z = theOdysseusMG1.transform.position.z - dist;
+		transform.position = temp;
+	}
+
+	void RightOffSet()
+	{
+		Vector3 temp = new Vector3 ();
+		temp.x = theOdysseusMG1.transform.position.x - 3.0f;
+		temp.y = theOdysseusMG1.transform.position.y;
 		temp.z = theOdysseusMG1.transform.position.z - dist;
 		transform.position = temp;
 	}
--- FYP/Assets/Scripts/Minigame/C5/FindEumaeus's/Rotate.cs
+++ FYP/Assets/Scripts/Minigame/C5/FindEumaeus's/Rotate.cs
@@ -5,6 +5,11 @@ public class Rotate : MonoBehaviour {
 	
 	public float xspeed = 0.0f;
 	public float yspeed = 0.0f;
+	public int state = 0;
+	//0 = down
+	//1 = right
+	//2 = up
+	//3 = down
 	//public float zspeed = 0.0f;
 	
 	// Use this for initialization
@@ -24,6 +29,11 @@ public class Rotate : MonoBehaviour {
 		angle.z += 90.0f;
 		rotation.eulerAngles = angle;
 		transform.localRotation = rotation;
+		if (state < 3)
+			state += 1;
+		else 
+			state = 0;
 		Invoke ("ObjRotate", Random.Range (1.0f, 3.0f));
+
 	}
 }
